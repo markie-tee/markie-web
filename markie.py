7d629c47cdba20fe49795104e23257678fbe71cb
markie tee <easymarkietee@proton.me>
2022-06-21 01:07:24 -0500

    Implement Config object for app settings
    found in config.py

--- markie.py
+++ markie.py
@@ -2,10 +2,14 @@
 
 # Markie Personal site
 
-version = "0.0.1"
+version = "0.1.1"
 
 from flask import Flask, render_template
+from config import Config
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def hello():
