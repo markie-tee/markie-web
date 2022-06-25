683e587e598cc1c99f78d98f2c1a09398897f440
markie tee <easymarkietee@proton.me>
2022-06-25 11:33:36 -0500

    Clean up config file

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     APP_NAME = 'markie web'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(basedir, 'markieWeb.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            #  'sqlite:///' + os.path.join(basedir, 'markieWeb.db')
+    #  SQLALCHEMY_TRACK_MODIFICATIONS = False
     #  MAIL_SERVER = os.environ.get('MAIL_SERVER')
     #  MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     #  MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
